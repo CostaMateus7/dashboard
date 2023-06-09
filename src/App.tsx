4fe9399c9af4483a107ee98bd48e482f47d54ec1
Mateus Costa <mateusscosta7@gmail.com>
2023-06-09 20:19:09 -0300

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
 function App() {
   return (
-    <>
-      <h1>Comanda Inteligente</h1>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,15 @@
-export {};
+import { Route, Routes, Navigate } from "react-router-dom";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route
+        path="pagina-inicial"
+        element={<h1>Página Inicial - Comanda Inteligente</h1>}
+      />
+      <Route path="*" element={<Navigate to={"pagina-inicial"} />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;

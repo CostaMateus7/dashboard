11ae53e1d0df685811248d6af29b126aced7cce3
Mateus Costa <mateusscosta7@gmail.com>
2023-06-11 20:50:08 -0300

    feat: add login screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,10 @@
-import { Button } from '@mui/material';
 import { Route, Routes, Navigate } from 'react-router-dom';
+import { Login } from '../shared/components/login/Login';
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route
-        path="pagina-inicial"
-        element={
-          <Button variant="contained" color="primary">
-            Cardápio Inteligente
-          </Button>
-        }
-      />
+      <Route path="pagina-inicial" element={<Login />} />
       <Route path="*" element={<Navigate to={'pagina-inicial'} />} />
     </Routes>
   );

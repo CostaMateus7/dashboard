f860b043ff349cb9522f10a7a108cac4be399a18
Mateus Costa <mateusscosta7@gmail.com>
2023-07-10 07:39:06 -0300

    delete: delete component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,9 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
-import { Login } from '../shared/components/login/Login';
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="pagina-inicial" element={<Login />} />
+      <Route path="pagina-inicial" />
       <Route path="*" element={<Navigate to={'pagina-inicial'} />} />
     </Routes>
   );

3efb75955ea9579e213588ee30f7cba56595d1ee
Mateus Costa <mateusscosta7@gmail.com>
2023-07-22 13:35:59 -0300

    style: add responsiveness to datailTools

--- src/pages/detailTools/DetailTools.tsx
+++ src/pages/detailTools/DetailTools.tsx
@@ -1,4 +1,20 @@
-import { Box, Button, Divider, Icon, Paper, useTheme } from '@mui/material';
+import { ArrowDropDown, ArrowDropUp } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Divider,
+  Icon,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+  Skeleton,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
+import { useState } from 'react';
 
 interface IDetailToolsProps {
   textNewButton?: string;
@@ -8,6 +24,12 @@ interface IDetailToolsProps {
   showDeleteButton?: boolean;
   showSaveButton?: boolean;
   showSaveAndCloseButton?: boolean;
+
+  showNewButtonLoading?: boolean;
+  showBackButtonLoading?: boolean;
+  showDeleteButtonLoading?: boolean;
+  showSaveButtonLoading?: boolean;
+  showSaveAndCloseButtonLoading?: boolean;
 
   onClickNew?: () => void;
   onClickDelete?: () => void;
@@ -24,6 +46,12 @@ export const DetailTools = ({
   showSaveButton = true,
   showSaveAndCloseButton = false,
 
+  showNewButtonLoading = false,
+  showBackButtonLoading = false,
+  showDeleteButtonLoading = false,
+  showSaveButtonLoading = false,
+  showSaveAndCloseButtonLoading = false,
+
   onClickNew,
   onClickDelete,
   onClickSave,
@@ -31,6 +59,13 @@ export const DetailTools = ({
   onClickBack,
 }: IDetailToolsProps) => {
   const theme = useTheme();
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'));
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleToogleButtonMenu: () => void = () => {
+    setIsOpen((prevOpen) => !prevOpen);
+  };
   return (
     <Box
       height={theme.spacing(5)}
@@ -42,7 +77,95 @@ export const DetailTools = ({
       gap={1}
       component={Paper}
     >
-      {showSaveButton && (
+      {mdDown && (
+        <ButtonGroup>
+          <Button>Selecionar Opções</Button>
+          <Button onClick={handleToogleButtonMenu}>
+            {isOpen ? <ArrowDropUp /> : <ArrowDropDown />}
+          </Button>
+        </ButtonGroup>
+      )}
+
+      {isOpen && !!mdDown && (
+        <Box
+          marginTop={32}
+          marginLeft={-31}
+          zIndex={5}
+          width={theme.spacing(30)}
+          gap={2}
+        >
+          <Paper>
+            <MenuList>
+              <MenuItem onClick={onClickSave}>
+                <Box display="flex" alignItems="center" gap={1}>
+                  <Icon>save</Icon>
+                  <Typography
+                    variant="button"
+                    whiteSpace="nowrap"
+                    textOverflow="ellipsis"
+                    overflow="hidden"
+                  >
+                    Salvar
+                  </Typography>
+                </Box>
+              </MenuItem>
+              <MenuItem onClick={onClickSaveAndClose}>
+                <Box display="flex" alignItems="center" gap={1}>
+                  <Icon>save</Icon>
+                  <Typography
+                    variant="button"
+                    whiteSpace="nowrap"
+                    textOverflow="ellipsis"
+                    overflow="hidden"
+                  >
+                    Salvar e Fechar
+                  </Typography>
+                </Box>
+              </MenuItem>
+              <MenuItem onClick={onClickDelete}>
+                <Box display="flex" alignItems="center" gap={1}>
+                  <Icon>delete</Icon>
+                  <Typography
+                    variant="button"
+                    whiteSpace="nowrap"
+                    textOverflow="ellipsis"
+                    overflow="hidden"
+                  >
+                    Apagar
+                  </Typography>
+                </Box>
+              </MenuItem>
+              <MenuItem onClick={onClickNew}>
+                <Box display="flex" alignItems="center" gap={1}>
+                  <Icon>add</Icon>
+                  <Typography
+                    variant="button"
+                    whiteSpace="nowrap"
+                    textOverflow="ellipsis"
+                    overflow="hidden"
+                  >
+                    {textNewButton}
+                  </Typography>
+                </Box>
+              </MenuItem>
+              <MenuItem onClick={onClickBack}>
+                <Box display="flex" alignItems="center" gap={1}>
+                  <Icon>arrow_back</Icon>
+                  <Typography
+                    variant="button"
+                    whiteSpace="nowrap"
+                    textOverflow="ellipsis"
+                    overflow="hidden"
+                  >
+                    Voltar
+                  </Typography>
+                </Box>
+              </MenuItem>
+            </MenuList>
+          </Paper>
+        </Box>
+      )}
+      {showSaveButton && !showSaveButtonLoading && !mdDown && (
         <Button
           color="primary"
           disableElevation
@@ -50,10 +173,19 @@ export const DetailTools = ({
           onClick={onClickSave}
           startIcon={<Icon>save</Icon>}
         >
-          Salvar
+          <Typography
+            variant="button"
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+            overflow="hidden"
+          >
+            Salvar
+          </Typography>
         </Button>
       )}
-      {showSaveAndCloseButton && (
+
+      {showSaveButtonLoading && !mdDown && <Skeleton width={100} height={60} />}
+      {showSaveAndCloseButton && !showSaveAndCloseButtonLoading && !mdDown && (
         <Button
           color="primary"
           disableElevation
@@ -61,10 +193,21 @@ export const DetailTools = ({
           onClick={onClickSaveAndClose}
           startIcon={<Icon>save</Icon>}
         >
-          Salvar e Fechar
+          <Typography
+            variant="button"
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+            overflow="hidden"
+          >
+            Salvar e Fechar
+          </Typography>
         </Button>
       )}
-      {showDeleteButton && (
+      {showSaveAndCloseButtonLoading && !mdDown && (
+        <Skeleton width={180} height={60} />
+      )}
+
+      {showDeleteButton && !showDeleteButtonLoading && !mdDown && (
         <Button
           color="primary"
           disableElevation
@@ -72,10 +215,21 @@ export const DetailTools = ({
           onClick={onClickDelete}
           startIcon={<Icon>delete</Icon>}
         >
-          Apagar
+          <Typography
+            variant="button"
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+            overflow="hidden"
+          >
+            Apagar
+          </Typography>
         </Button>
       )}
-      {showNewButton && (
+      {showDeleteButtonLoading && !mdDown && (
+        <Skeleton width={100} height={60} />
+      )}
+
+      {showNewButton && !showNewButtonLoading && !mdDown && (
         <Button
           color="primary"
           disableElevation
@@ -83,11 +237,20 @@ export const DetailTools = ({
           startIcon={<Icon>add</Icon>}
           onClick={onClickNew}
         >
-          {textNewButton}
+          <Typography
+            variant="button"
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+            overflow="hidden"
+          >
+            {textNewButton}
+          </Typography>
         </Button>
       )}
-      <Divider variant="middle" orientation="vertical" />
-      {showBackButton && (
+      {showNewButtonLoading && !mdDown && <Skeleton width={100} height={60} />}
+
+      {!mdDown && <Divider variant="middle" orientation="vertical" />}
+      {showBackButton && !showBackButtonLoading && !mdDown && (
         <Button
           color="primary"
           disableElevation
@@ -95,9 +258,17 @@ export const DetailTools = ({
           onClick={onClickBack}
           startIcon={<Icon>arrow_back</Icon>}
         >
-          Voltar
+          <Typography
+            variant="button"
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+            overflow="hidden"
+          >
+            Voltar
+          </Typography>
         </Button>
       )}
+      {showBackButtonLoading && !mdDown && <Skeleton width={100} height={60} />}
     </Box>
   );
 };
